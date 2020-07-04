522b35912baa079d0d98ad059fee233979755a1a
bigdaddymax <mmaxbb@gmail.com>
2020-07-04 16:04:54 -0700

    Fix rectangle size

--- detect.py
+++ detect.py
@@ -59,7 +59,7 @@ class detector:
         endY    = int(pos.bottom())
         cv2.rectangle(frame, (startX, startY), (endX, endY), self.objColor[idx], 2)
         textSize, baseline = cv2.getTextSize( self.label + ' ' + str(box['confidence']), cv2.FONT_HERSHEY_SIMPLEX, 0.45, 2)
-        cv2.rectangle(frame, (box['box'][0], box['box'][1]) , (box['box'][0] + textSize[0], box['box'][1] - textSize[1]), self.objColor[idx], -1)
+        cv2.rectangle(frame, (startX, startY) , (startX + textSize[0], startY - textSize[1]), self.objColor[idx], -1)
         cv2.putText(frame, self.label, (startX, startY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
         return frame
 
